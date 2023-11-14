5f204103a4a0064dcffd5d6c50e28594eca3b833
juan-sotelo1994 <ing.davidsotelo19@gmail.com>
2023-11-13 21:51:48 -0500

    ver5

--- app.py
+++ app.py
@@ -3,25 +3,33 @@ import os
 
 app = Flask(__name__)
 
-##@app.route('/')
-##def index():
-    #return "que maricada"
-  ##  recursos =['PHP','PYTHON','JAVA', 'DART']
-    ##data = {
-       ## 'titulo':'gil',
-       ## 'bienvenida':'!saludos!',
-       ## 'recursos':recursos,
-      ##  'numero_recursos': len(recursos)
-    ##}
 @app.route('/')
-def pagina1():
-    return render_template('pagina1.html')
+def index():
+    #return "si funciono"
+    #recursos =['PHP','PYTHON','JAVA', 'DART']
+    #data = {
+     #   'titulo':'gil',
+     #   'bienvenida':'!saludos!',
+    #  'recursos':recursos,
+     #   'numero_recursos': len(recursos)
+    #}    
+    #return render_template('index.html', data=data)
 
-@app.route('/pagina2')
-def pagina2():
-    return render_template('pagina2.html')
 
 
-   ## return render_template('index.html', data=data)
+    return render_template('index.html')
+
+@app.route('/reservas')
+def reservas():
+    return render_template('reservas.html')
+
+@app.route('/novedades')
+def novedades():
+    return render_template('novedades.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 8081, debug=True)
