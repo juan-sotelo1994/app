fe62f2f80a2167be59e97284f87f9b95e6d192b9
juan-sotelo1994 <ing.davidsotelo19@gmail.com>
2023-11-13 22:10:10 -0500

    ver7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -27,8 +27,14 @@ def reservas():
 def novedades():
     return render_template('novedades.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Aquí puedes agregar la lógica de autenticación con el correo y la contraseña
+        # Por ahora, redirigimos a la página de inicio después del "login"
+        return redirect(url_for('index'))
     return render_template('login.html')
 
 if __name__ == "__main__":
